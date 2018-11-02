c389ff067aec20269e4245cb64b8328905f2134b
AntonVoronezh <pstut@mail.ru>
2018-11-02 22:36:09 +0300

    12 #6

--- 12/task.js
+++ 12/task.js
@@ -1,1 +1,9 @@
+const uniq = (arg) => arg.filter((item, i, arr) => i === arr.indexOf(item));
+
+const intersection = (arg1, arg2) => {
+  const result = arg1.filter(item => arg2.indexOf(item) !== -1);
+  return uniq(result);
+};
+
+export default intersection;
 
